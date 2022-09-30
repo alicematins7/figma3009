7085a73f3da65984daa9c6a554527cc4cf26f109
Alice Martins <102804150+alicematins7@users.noreply.github.com>
2022-09-30 15:33:13 -0300

    Add files via upload

---
+++ am/alicemartins/src/services/data/User/index.ts
@@ -0,0 +1,12 @@
+import { IResponseUser, IUser} from "interfaces/user.interface";
+import api from "services/data";
+
+class UserData {
+    register(data: IUser) {
+        return api.post<IResponseUser>('/register', data)
+    }
+    login(data: IUser){
+        return api.post<IResponseUser>('/login', data)
+    }
+}
+export default new UserData()
